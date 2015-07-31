407274dd5befd3d70e283d258c1a4b060e56333e
ejarzo <ejarz25@gmail.com>
2015-07-30 21:01:33 -0400

    redesign partially complete of landing page and sign in page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,5 @@
 $( document ).ready(function() {
 
-$( "#signup" ).click(function() {
-    $("#header").animate({
-        "padding-top": "10px"
-    },0, function() {});
-    $("#buttons").hide();
-    $("#signup-page").fadeIn();
-});
-
-$( ".signin" ).click(function() {
-    $("#buttons").hide();
-    $("#signin-page").fadeIn();
-    $("#header").animate({
-        "padding-top": "10px"
-    },0, function() {});
-});
-
 var resize = function() {
     var x = $(window).height();
     var y = x - 120 - 482;
